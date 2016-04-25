25ce9d7d71048476d006fd60bf856922c2313383
faridpark12 <faridpark12@gmail.com>
2016-04-25 15:25:02 +0900

    initial commit

---
+++ app/src/main/java/pumpkin/kr/hanbat/MainActivity.java
@@ -0,0 +1,13 @@
+package pumpkin.kr.hanbat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
