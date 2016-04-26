ad3dd39e39608cd52ffbe6eaf4b870b5f98890eb
faridpark12 <faridpark12@gmail.com>
2016-04-26 21:26:41 +0900

    Fragment 대신 Activity 로 변경

---
+++ app/src/main/java/pumpkin/kr/hanbat/MapActivity.java
@@ -0,0 +1,9 @@
+package pumpkin.kr.hanbat;
+
+import com.nhn.android.maps.NMapActivity;
+
+/**
+ * Created by farid on 2016-04-26.
+ */
+public class MapActivity extends NMapActivity {
+}
---
+++ app/src/main/java/pumpkin/kr/hanbat/NotificationActivity.java
@@ -0,0 +1,9 @@
+package pumpkin.kr.hanbat;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by farid on 2016-04-26.
+ */
+public class NotificationActivity extends AppCompatActivity {
+}
