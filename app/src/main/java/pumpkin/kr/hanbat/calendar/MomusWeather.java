d11274165746157a6c34322c2c2b397a546ef6a8
faridpark12 <faridpark12@gmail.com>
2016-05-01 17:44:08 +0900

    달력 커밋

---
+++ app/src/main/java/pumpkin/kr/hanbat/calendar/MomusWeather.java
@@ -0,0 +1,17 @@
+package pumpkin.kr.hanbat.calendar;
+
+/**
+ * Weather Info.<br>
+ * 
+ * Created by brownsoo
+ */
+public class MomusWeather {
+
+    public static enum Weather {
+        SUNSHINE, 
+        SUN_CLOUND, 
+        CLOUDY, 
+        RAINNY, 
+        SNOW
+    }
+}
